6308bc1ce00bc045c66342018a5031f3c5d74172
Daniel S. Billing <daniel.billing@intility.no>
2021-09-21 17:26:52 +0200

    Added fortify and jetstream route files

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,6 +46,12 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            Route::namespace($this->namespace)
+                ->group(base_path('routes/jetstream.php'));
+
+            Route::namespace($this->namespace)
+                ->group(base_path('routes/fortify.php'));
         });
     }
 
