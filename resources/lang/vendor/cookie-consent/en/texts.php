5fbfdd0c063a1b6277ba421b726bf0d344ff5662
Daniel S. Billing <daniel@infihex.com>
2021-12-29 17:27:27 +0100

    Update cookie consent lang files

---
+++ resources/lang/vendor/cookie-consent/en/texts.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'message' => 'We use cookies which are stored in your browser when you visit our website. By continuing to visit our website or using our services, you accept that we use cookies.',
+    'agree' => 'I understand',
+];
