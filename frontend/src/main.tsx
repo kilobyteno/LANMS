3e1e1d0a0a33a82b48b79ecec87bfde54c5d5572
Daniel S. Billing <daniel@kilobyte.no>
2026-04-14 10:39:16 +0200

    Bump frontend deps; update calendar and main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './i18n';
 import './index.css';
+import 'react-day-picker/style.css';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
